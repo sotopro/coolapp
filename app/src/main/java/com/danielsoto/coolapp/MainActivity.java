38f51a42efd98f74976a68f802924cef330567b6
Daniel Soto <dasg.1993@gmail.com>
2022-01-03 11:41:04 -0500

    [feature] - add function to pick from camera & validate permission in to camera & storage

--- app/src/main/java/com/danielsoto/coolapp/MainActivity.java
+++ app/src/main/java/com/danielsoto/coolapp/MainActivity.java
@@ -1,14 +1,102 @@
 package com.danielsoto.coolapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView set;
+    ProgressDialog pd;
+    private static final int CAMERA_REQUEST = 100;
+    private static final int STORAGE_REQUEST = 200;
+    private static final int IMAGE_PICK_GALLERY_REQUEST = 300;
+    private static final int IMAGE_PICK_CAMERA_REQUEST = 400;
+
+    String cameraPermission[];
+    String storagePermission[];
+    Uri imageUri;
+    String profileOrCoverImage;
+    AppCompatButton editImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editImage = findViewById(R.id.edit_image);
+        set = findViewById(R.id.profile_image);
+        pd = new ProgressDialog(this);
+        pd.setCanceledOnTouchOutside(false);
+        cameraPermission = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        storagePermission = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+        editImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pd.setMessage("Updating Profile Picture");
+                profileOrCoverImage = "Image";
+                showImagePicDialog();
+            }
+        });
     }
+
+    private void showImagePicDialog() {
+        String options[] = {"Camera", "Gallery"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Pick image from");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (which == 0) {
+                    if(!checkCameraPermission()) {
+                        requestCameraPermission();
+                    } else {
+                        pickFromCamera();
+                    }
+                }
+//                else if(which == 1) {
+//                    if(!checkStoragePermission()) {
+//                        requestStoragePermission();
+//                    } else {
+//                        pickFromGallery();
+//                    }
+//                }
+            }
+        });
+        builder.create().show();
+    }
+
+    private Boolean checkCameraPermission() {
+        boolean result = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == (PackageManager.PERMISSION_GRANTED);
+        boolean result1 = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+
+        return result && result1;
+    }
+
+    private void requestCameraPermission() {
+        requestPermissions(cameraPermission, CAMERA_REQUEST);
+    }
+
+    private void pickFromCamera() {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.Images.Media.TITLE, "Temp_pic");
+        contentValues.put(MediaStore.Images.Media.DESCRIPTION, "Temp description");
+        imageUri = this.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(cameraIntent, IMAGE_PICK_CAMERA_REQUEST);
+    }
+
 }
